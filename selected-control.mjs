36815faf50ca9374beedc3533122cf73630d954c
Tim Erickson <tim@in2words.org>
2023-04-15 20:31:31 -0700

    - Create none/item/relationship panes in selection box - Make selection box switch panes with selection - Move SelectedControl style to discrete css file

--- selected-control.mjs
+++ selected-control.mjs
@@ -5,11 +5,21 @@ import {
 } from 'events';
 import { Types } from 'data';
 
+const PARENT_BLOCK_CLASS = "selected-control";
+const NONE_DIV_CLASS = `${PARENT_BLOCK_CLASS}__none`;
+const ITEM_DIV_CLASS = `${PARENT_BLOCK_CLASS}__item`;
+const RELATIONSHIP_DIV_CLASS = `${PARENT_BLOCK_CLASS}__relationship`;
+const VISIBLE_CLASS = `${PARENT_BLOCK_CLASS}__visible`;
+
 export class SelectedControl extends Component {
     state = {
         selectionType: '',
         selection: []
     }
+
+    boxNone = undefined
+    boxItem = undefined
+    boxRelationshp = undefined
 
     constructor(props) {
         super(props);
@@ -22,6 +32,7 @@ export class SelectedControl extends Component {
 
     onSelected({ type, id }) {
         let selection;
+        const oldType = this.state.selectionType;
         if (type === Types.ITEM) {
             selection = this.data.getItem(id);
         } else if (type === Types.RELATIONSHIP) {
@@ -33,12 +44,49 @@ export class SelectedControl extends Component {
         this.setState({
             selectionType: type,
             selection: this.state.selection.concat(selection)
+        }, () => {
+            // console.debug('switch selected', oldType, type);
+            this.switchBoxes(oldType, type);
         });
     }
 
+    switchBoxes(oldType, newType) {
+        if (newType === oldType) {
+            return;
+        }
+        this.setVisibility(oldType, false);
+        this.setVisibility(newType, true);
+    }
+
+    setVisibility(type, visible) {
+        let box;
+        switch (type) {
+            case Types.ITEM:
+                box = this.boxItem;
+                break;
+            case Types.RELATIONSHIP:
+                box = this.boxRelationshp;
+                break;
+            default:
+                box = this.boxNone;
+        }
+        if (visible) {
+            box.classList.add(VISIBLE_CLASS);
+        } else {
+            box.classList.remove(VISIBLE_CLASS);
+        }
+    }
+
     onDeselected({ id }) {
+        const oldType = this.state.selectionType;
+        const newSelection = this.state.selection.filter(x => x.id !== id);
+        const newType = newSelection.length ? newSelection[0].type : 'none';
         this.setState({
-            selection: this.state.selection.filter(x => x.id !== id)
+            selectionType: newType,
+            selection: newSelection
+        }, () => {
+            // console.debug('switch deselected', oldType, newType);
+            this.switchBoxes(oldType, newType);
         });
     }
 
@@ -47,13 +95,20 @@ export class SelectedControl extends Component {
         if (!selection.length) {
             selectionType = 'none';
         }
-        return `${selectionType}${(selection.length > 1) ? 's' : ''} ${selection.map(x => x.id).join(',')}`;
+        return `${selection.map(x => x.id).join(',')}`;
     }
 
     render() {
         return html`
-            <p>Selection</p>
-            ${this.selectionName()}
+            <div ref=${e => this.boxNone = e} class="${NONE_DIV_CLASS} ${VISIBLE_CLASS}">
+                --- no selection ---
+            </div>
+            <div ref=${e => this.boxItem = e} class=${ITEM_DIV_CLASS}>
+                item ${this.selectionName()}
+            </div>
+            <div ref=${e => this.boxRelationshp = e} class=${RELATIONSHIP_DIV_CLASS}>
+                relationship ${this.selectionName()}
+            </div>
         `;
     }
 }
